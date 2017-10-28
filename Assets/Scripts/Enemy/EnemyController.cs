cbac021edb14bda073f4eabb50cce0ecd275b8c4
tuckrr <tuck@Christophers-MacBook-Pro-4.local>
2017-10-27 22:33:12 -0400

    enemy + slime controllers

---
+++ Assets/Scripts/Enemy/EnemyController.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour {
+
+	public float speed;
+	public float damageOnHit;
+	public int xp;
+	public PlayerController player;
+
+	private Rigidbody2D rb;
+
+	void Start() {
+		rb = GetComponent<Rigidbody2D>();
+	}
+
+	// act as callbacks in the 'ai' of the more specific enemies
+
+	public void MoveUp() { 
+		MoveDegree(90.0f);
+	}
+
+	public void MoveDown() {
+		MoveDegree(270.0f);
+	}
+
+	public void MoveRight() {
+		MoveDegree(0.0f);
+	}
+
+	public void MoveLeft() {
+		MoveDegree(180.0f);
+	}
+
+	public void MoveDegree(float angle) {
+		MoveRadian(Mathf.Deg2Rad * angle);
+	}
+
+	public void MoveRadian(float angle) {
+		rb.MoveRotation((angle * Mathf.Rad2Deg - 90) % 360);
+		rb.velocity = new Vector2(Mathf.Cos(angle) * speed, Mathf.Sin(angle) * speed);
+	}
+
+	void OnCollisionEnter2D(Collision2D other) {
+		if (other.gameObject.CompareTag("Player")) {
+			HurtPlayer(damageOnHit);
+		}
+	}
+
+	public void HurtPlayer(float dmg) {
+		player.TakeDamage(dmg);
+	}
+
+	public void Die() {
+		player.AddExp(xp);
+	}
+}
---
+++ Assets/Scripts/Enemy/SlimeController.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SlimeController : EnemyController {
+	// Update is called once per frame
+	void FixedUpdate() {
+		MoveRadian(pickAngle());
+	}
+
+	private float pickAngle() {
+		Vector2 dir = player.GetComponent<Rigidbody2D>().position - GetComponent<Rigidbody2D>().position;
+		dir.Normalize();
+		return Mathf.Atan2(dir.y, dir.x);
+	}
+}
