80c8ee6654647dd4a0304f751e3f1f5447598419
aserv <alexs9@vt.edu>
2017-10-27 22:32:50 -0400

    Add PlayerController and BaseItem

---
+++ Assets/Scripts/Pickup.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Collider2D))]
+public class Pickup : MonoBehaviour {
+
+    private void Start() {
+        item = new HealthUpItem();
+    }
+
+    public BaseItem item;
+    private void OnTriggerEnter2D(Collider2D collider) {
+        if (collider.CompareTag("Player")) {
+            if (collider.GetComponent<PlayerController>().PickupItem(item)) {
+                Destroy(gameObject);
+            }
+        }
+    }
+}
